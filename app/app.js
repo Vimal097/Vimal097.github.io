55f7490cd7f8fd9e74e4eedc499be9f83c8b1321
Vimal097 <vimalrajc101@gmail.com>
2023-11-08 23:50:56 +0530

    Sample HTML File with images

---
+++ app/app.js
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Dance Academy</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Your Dance Academy</h1>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#classes">Classes</a></li>
+                <li><a href="#events">Events</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+        <h2>Welcome to Your Dance Academy</h2>
+        <p>Learn to dance and express yourself through the art of movement.</p>
+    </section>
+
+    <section id="about">
+        <h2>About Us</h2>
+        <p>Our dance academy is dedicated to promoting the joy of dancing and providing professional dance training.</p>
+    </section>
+
+    <section id="classes">
+        <h2>Classes</h2>
+        <p>Explore our range of dance classes, from classical to contemporary, for all age groups and skill levels.</p>
+    </section>
+
+    <section id="events">
+        <h2>Upcoming Events</h2>
+        <p>Stay updated on our latest dance events and performances.</p>
+    </section>
+
+    <section id="contact">
+        <h2>Contact Us</h2>
+        <p>If you have any questions or would like to enroll in our classes, please get in touch with us.</p>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Your Dance Academy</p>
+    </footer>
+</body>
+</html>
